c58fbb549b3e1206ebfdeb870b52b1c8a5a3e97c
iko <ilyakooo0@gmail.com>
2019-02-18 10:49:23 +0300

    Initial

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $( document ).ready(function() {
     let name = window.location.href.split("#")[1];
     
     if (name === undefined || name == "") {
-        $("#sosi").text(decodeURI(name) + ", соси пожалуйста");
-        document.title = decodeURI(name) + ", соси пожалуйста";
-    } else {
         $("#sosi").text("соси пожалуйста");
         document.title = "соси пожалуйста";
+    } else {
+        $("#sosi").text(decodeURI(name) + ", соси пожалуйста");
+        document.title = decodeURI(name) + ", соси пожалуйста";
     }
 });
